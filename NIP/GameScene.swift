309cc025b3fd7c60b43f3ad251d379797cdef03e
admin <admin@admins-macbook-pro-2.local>
2016-05-08 16:57:14 +0700

    hello

--- NIP/GameScene.swift
+++ NIP/GameScene.swift
@@ -8,38 +8,221 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+struct PhysicsCategory {
+    static let None      : UInt32 = 0
+    static let All       : UInt32 = UInt32.max
+    static let badMonster   : UInt32 = 0b1 // 1
+    static let goodMonster : UInt32 = 0b100
+    static let Projectile: UInt32 = 0b10      // 2
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    let player = SKSpriteNode(imageNamed: "player")
+    
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        backgroundColor = SKColor.grayColor()
+        player.position = CGPoint(x: self.frame.width * 0.5 , y: self.frame.height * 0.1)
+        addChild(player)
         
-        self.addChild(myLabel)
+        
+        physicsWorld.gravity = CGVectorMake(0, 0)
+        physicsWorld.contactDelegate = self
+        
+        
+        
+        //MARK: Monster Configure ***************************************************************************************
+        
+        
+        func addMonster() {
+            // Create sprite
+            let badmonster = SKSpriteNode(imageNamed: "BadMonster")
+            // Determine where to spawn the monster along the Y axis
+            badmonster.physicsBody = SKPhysicsBody(rectangleOfSize: badmonster.size) // 1
+            badmonster.physicsBody?.dynamic = true // 2
+            badmonster.physicsBody?.categoryBitMask = PhysicsCategory.badMonster // 3
+            badmonster.physicsBody?.contactTestBitMask = PhysicsCategory.Projectile // 4
+            badmonster.physicsBody?.collisionBitMask = PhysicsCategory.None // 5
+            
+            
+            let actualY = random(min: badmonster.size.height * 2, max: size.height - badmonster.size.height * 2)
+            // Position the monster slightly off-screen along the right edge,
+            // and along a random position along the Y axis as calculated above
+            badmonster.position = CGPoint(x: size.width + badmonster.size.width/2, y: actualY)
+            // Add the monster to the scene
+            addChild(badmonster)
+            // Determine speed of the monster
+            let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+            let actualDurationForGoodMonster = random(min: CGFloat(2.0), max: CGFloat(4.0))
+            // Create the actions
+            let actionMove = SKAction.moveTo(CGPoint(x: -badmonster.size.width/2, y: actualY), duration: NSTimeInterval(actualDuration))
+            let actionMoveDone = SKAction.removeFromParent()
+            badmonster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+        }
+        
+        func addGoodMonster() {
+            let goodmonster = SKSpriteNode(imageNamed: "GoodMonster")
+            
+            goodmonster.physicsBody = SKPhysicsBody(rectangleOfSize: goodmonster.size) // 1
+            goodmonster.physicsBody?.dynamic = true // 2
+            goodmonster.physicsBody?.categoryBitMask = PhysicsCategory.goodMonster // 3
+            goodmonster.physicsBody?.contactTestBitMask = PhysicsCategory.Projectile // 4
+            goodmonster.physicsBody?.collisionBitMask = PhysicsCategory.None // 5
+
+            
+            let goodactualY = random(min: goodmonster.size.height * 2, max: size.height - goodmonster.size.height * 2)
+            goodmonster.position = CGPoint(x: size.width + goodmonster.size.width/2, y: goodactualY)
+            addChild(goodmonster)
+            let actualDurationForGoodMonster = random(min: CGFloat(2.0), max: CGFloat(4.0))
+            
+            let GoodactionMove = SKAction.moveTo(CGPoint(x: -goodmonster.size.width/2, y: goodactualY), duration: NSTimeInterval(actualDurationForGoodMonster))
+            let actionMoveDone = SKAction.removeFromParent()
+            goodmonster.runAction(SKAction.sequence([GoodactionMove, actionMoveDone]))
+            
+        }
+
+        
+        runAction(SKAction.repeatActionForever(
+            SKAction.sequence([
+                SKAction.runBlock(addMonster),
+                SKAction.waitForDuration(1.0)
+                ])
+            ))
+        
+        runAction(SKAction.repeatActionForever(
+            SKAction.sequence([
+                SKAction.runBlock(addGoodMonster),
+                SKAction.waitForDuration(1.0)
+                ])
+            ))
+        
+        
+        
+        
+        //MARK: *************************************************************************************************************
+        
+        
+        
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        // 1 - Choose one of the touches to work with
+        guard let touch = touches.first else {
+            return
         }
+        let touchLocation = touch.locationInNode(self)
+        
+        // 2 - Set up initial location of projectile
+        let projectile = SKSpriteNode(imageNamed: "projectile")
+        projectile.position = player.position
+        projectile.physicsBody = SKPhysicsBody(circleOfRadius: projectile.size.width/2)
+        projectile.physicsBody?.dynamic = true
+        projectile.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
+        projectile.physicsBody?.contactTestBitMask = PhysicsCategory.goodMonster
+        projectile.physicsBody?.contactTestBitMask = PhysicsCategory.badMonster
+        projectile.physicsBody?.collisionBitMask = PhysicsCategory.None
+        projectile.physicsBody?.usesPreciseCollisionDetection = true
+        
+        // 3 - Determine offset of location to projectile
+        let offset = touchLocation - projectile.position
+        
+        // 4 - Bail out if you are shooting down or backwards
+        if (offset.y < 0) { return }
+        
+        // 5 - OK to add now - you've double checked position
+        addChild(projectile)
+        
+        // 6 - Get the direction of where to shoot
+        let direction = offset.normalized()
+        
+        // 7 - Make it shoot far enough to be guaranteed off screen
+        let shootAmount = direction * 1000
+        
+        // 8 - Add the shoot amount to the current position
+        let realDest = shootAmount + projectile.position
+        
+        // 9 - Create the actions
+        let actionMove = SKAction.moveTo(realDest, duration: 2.0)
+        let actionMoveDone = SKAction.removeFromParent()
+        projectile.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+        
+    }
+    
+    func projectileDidCollideWithMonster(projectile:SKSpriteNode, monster:SKSpriteNode) {
+        print("Hit")
+        projectile.removeFromParent()
+        monster.removeFromParent()
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        
+        // 1
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        // 2
+        if ((firstBody.categoryBitMask & PhysicsCategory.badMonster != 0) &&
+            (secondBody.categoryBitMask & PhysicsCategory.Projectile != 0)) {
+            projectileDidCollideWithMonster(firstBody.node as! SKSpriteNode, monster: secondBody.node as! SKSpriteNode)
+        }
+        
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
 }
+
+
+func random() -> CGFloat {
+    return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+}
+
+func random(min min: CGFloat, max: CGFloat) -> CGFloat {
+    return random() * (max - min) + min
+}
+
+
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+#if !(arch(x86_64) || arch(arm64))
+    func sqrt(a: CGFloat) -> CGFloat {
+        return CGFloat(sqrtf(Float(a)))
+    }
+#endif
+
+extension CGPoint {
+    func length() -> CGFloat {
+        return sqrt(x*x + y*y)
+    }
+    
+    func normalized() -> CGPoint {
+        return self / length()
+    }
+}
+
